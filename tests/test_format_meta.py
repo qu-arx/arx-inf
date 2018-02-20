bb60f6479e37e1bb4ff26598a4462cc4e8f3f2fc
Louis <louismmx@gmail.com>
2018-02-20 22:46:34 +0000

    Use proper unit testing

--- tests/test_format_meta.py
+++ tests/test_format_meta.py
@@ -12,20 +12,23 @@ class AuthTestSuite(unittest.TestCase):
         Confirm the default value of ``style`` parameter in ``FormatAuth``
         is set to "summary".
         """
-        assert FormatAuth(["Test"]) == FormatAuth(["Test"], style="summary")
+        self.assertEqual(FormatAuth(["Test"]), \
+                         FormatAuth(["Test"], style="summary"))
         
     def test_one_author(self):
-        assert FormatAuth(["Hello World"]) == "World"
+        self.assertEqual(FormatAuth(["Hello World"]), "World")
 
     def test_two_authors(self):
-        assert FormatAuth(["Hello World", "Foo B. Baz"]) == "World & Baz"
+        self.assertEqual(FormatAuth(["Hello World", "Foo B. Baz"]), \
+                         "World & Baz")
 
     def test_three_authors(self):
-        assert FormatAuth(["Hello World", "Foo B. Baz",
-        "Homer J. Simpson"]) == "World et al."
+        self.assertEqual(FormatAuth(["Hello World", "Foo B. Baz",
+                         "Homer J. Simpson"]), "World et al.")
 
     def test_filename_auth_format(self):
-        assert FormatAuth(["Hello World"], style="filename") == "world"
+        self.assertEqual(FormatAuth(["Hello World"], style="filename"), \
+                         "world")
 
 class MetaTestSuite(unittest.TestCase):
     """
@@ -33,12 +36,13 @@ class MetaTestSuite(unittest.TestCase):
     """
 
     def test_tweet_format(self):
-        assert FormatMeta(test_metadata) == "Welt et al. (⑳) Testing " \
-        + "(arX⠶2001.12345v1［CS.AI］) http://arxiv.org/abs/2001.12345v1"
+        self.assertEqual(FormatMeta(test_metadata),
+            "Welt et al. (⑳) Testing (arX⠶2001.12345v1［CS.AI］)" + \
+            " http://arxiv.org/abs/2001.12345v1")
 
     def test_log_format(self):
-        assert FormatMeta(test_metadata, to="log") == \
-        "- [ ] welt20 ⠶ [Testing](http://arxiv.org/abs/2001.12345v1)"
+        self.assertEqual(FormatMeta(test_metadata, to="log"),
+        "- [ ] welt20 ⠶ [Testing](http://arxiv.org/abs/2001.12345v1)")
 
 if __name__ == '__main__':
     unittest.main()
